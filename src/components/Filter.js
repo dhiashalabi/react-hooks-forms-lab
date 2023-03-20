587b0cccd598433730025175aec87bb000796995
Dhia' Alhaq Shalabi <dhia.shalabi@gmail.com>
2023-03-20 19:18:55 +0300

    feat: completed assignment

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React from 'react';
 
-function Filter({ onCategoryChange }) {
+function Filter({ search, onSearchChange, onCategoryChange }) {
+  function handleSearchChange(event) {
+    onSearchChange(event.target.value);
+  }
+
   return (
-    <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
-      <select name="filter" onChange={onCategoryChange}>
-        <option value="All">Filter by category</option>
-        <option value="Produce">Produce</option>
-        <option value="Dairy">Dairy</option>
-        <option value="Dessert">Dessert</option>
+    <div className='Filter'>
+      <input
+        type='text'
+        name='search'
+        placeholder='Search...'
+        value={search}
+        onChange={handleSearchChange}
+      />
+      <select
+        name='filter'
+        onChange={onCategoryChange}
+      >
+        <option value='All'>Filter by category</option>
+        <option value='Produce'>Produce</option>
+        <option value='Dairy'>Dairy</option>
+        <option value='Dessert'>Dessert</option>
       </select>
     </div>
   );
